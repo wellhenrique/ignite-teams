fecf06d2d3b0bd455fc4a333a3b29e8e9b9b4f0e
wellhenrique <welissonh80@gmail.com>
2022-11-01 19:15:30 -0300

    feat: fladList for groups created

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,25 @@
+import { useState } from 'react'
+
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
 
 import { Container } from './styles'
+import { FlatList } from 'react-native'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Amigos'])
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" onPress={() => ({})} />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   )
 }
