0071fa17f6ddf0dd306c04902106b493e0e48801
wellhenrique <welissonh80@gmail.com>
2022-11-01 19:32:57 -0300

    feat: returns an empty list when not groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
+import { FlatList } from 'react-native'
 
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
+import { ListEmpty } from '@components/ListEmpty'
 
 import { Container } from './styles'
-import { FlatList } from 'react-native'
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Amigos'])
+  const [groups, setGroups] = useState<string[]>([])
 
   return (
     <Container>
@@ -19,6 +20,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   )
