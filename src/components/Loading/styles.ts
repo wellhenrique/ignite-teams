80dd9bf074124165f08dc30118ebd726ecc50832
wellhenrique <welissonh80@gmail.com>
2022-11-03 09:51:57 -0300

    refactor: refactor prop styled-components

--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native'
 
 export const Container = styled.View`
   flex: 1;
@@ -6,8 +6,8 @@ export const Container = styled.View`
   align-items: center;
 
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
-`;
+`
 
 export const LoadIndicator = styled.ActivityIndicator.attrs(({ theme }) => ({
-  color: theme.COLORS.GREEN_700
-}))``;
+  color: theme.COLORS.GREEN_700,
+}))``
